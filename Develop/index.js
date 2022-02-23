78ee10b3a0cbaba15775b64d4739d94f751006a9
Blerand Ismaili <94268979+BlerandIsm@users.noreply.github.com>
2022-02-23 18:21:21 -0500

    initial questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,37 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
+const fs = require('fs');
+const { type } = require('os');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// An array of questions for user input
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'Enter the title of the project : (Required)',
+    validate: titleInput => {
+        if (titleInput){
+            return true;
+        } else{
+            console.log('Please enter a title to continue!');
+            return false
+        }
+    } 
+},
+{
+    type: 'input',
+    name: 'description',
+    message : 'Give a brief description of your project (Required)',
+    validate: descriptionInput =>{
+        if (descriptionInput){
+            return true;
+        } else {
+            console.log('Please give a description for your project');
+            return false ;
+        }
+    }
+}
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
